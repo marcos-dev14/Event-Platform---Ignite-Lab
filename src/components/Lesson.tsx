f0bcd59763144bf2855bcaaf729ac3e3f8915da4
marcos-dev14 <mprogramador.10@gmail.com>
2022-06-24 15:34:59 -0300

    Ajutes no Layout do Lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,8 @@
+import classNames from 'classnames';
 import { format, isPast } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { CheckCircle, Lock } from 'phosphor-react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -10,27 +11,30 @@ interface LessonProps {
   type: 'live' | 'class';
 }
 
-export function Lesson({
-  title,
-  slug,
-  availableAt,
-  type
-}: LessonProps) {
-  const isLessonAvailable = isPast(availableAt);
-  const availableDateFormatted = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+export function Lesson(props: LessonProps) {
+  const { slug } = useParams<{ slug: string }>() 
+
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR,
   })
 
+  const isActiveLesson = slug === props.slug;
+
   return (
-    <Link to={`/event/lesson/${slug}`} className="group">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">
         {availableDateFormatted[0].toUpperCase() + availableDateFormatted.substr(1)}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
+      <div className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors', 
+      { 'bg-green-500': isActiveLesson })
+      }>
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <span className={classNames('text-sm text-blue-500 font-medium flex items-center gap-2', {
+              'text-gray-100' : isActiveLesson
+            })}>
               <CheckCircle size={20} />
               Conteúdo liberado
               </span>
@@ -40,13 +44,17 @@ export function Lesson({
               Em breve
             </span>
           )}
-          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
-            {type === 'live' ? 'AO VIVO' : 'AULA PRATICA'}
+          <span className={classNames('text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold', {
+            'border-gray-100' : isActiveLesson
+          })}>
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRATICA'}
           </span>
         </header>
 
-        <strong className="text-gray-200 mt-5 block">
-          {title}
+        <strong className={classNames('text-gray-200 mt-5 block', {
+          'text-gray-100' : isActiveLesson
+        })}>
+          {props.title}
         </strong>
       </div>
     </Link>
